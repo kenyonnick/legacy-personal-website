4c48b62ee5bd03d96143bb3a07acf1dea9e6979d
kenyonnick <jusisgood45@gmail.com>
2020-12-11 08:54:07 -0500

    Add links to navbar

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,14 +1,21 @@
 import React from 'react'
 
 class NavBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.worksLink = "/";
+        this.githubLink = "https://www.github.com/kenyonnick";
+        this.aboutLink = "/about.html";
+    }
+
     render() {
         return (
             <div id="nav">
                 <h1>Nicklas Kenyon</h1>
                 <div id="nav-items">
-                    <h2 className="nav-item">Games</h2>
-                    <h2 className="nav-item">Artwork</h2>
-                    <h2 className="nav-item">About</h2>
+                    <h2 className="nav-item"><a href={ this.worksLink }>Works</a></h2>
+                    <h2 className="nav-item"><a href={ this.githubLink } target="blank">GitHub</a></h2>
+                    <h2 className="nav-item"><a href={ this.aboutLink }>About</a></h2>
                 </div>
             </div>
         );
